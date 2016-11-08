a00bd090c72d22f96ee41552b11260e7224be7b3
Selma Boudissa <sb091044@creusot.u-bourgogne.fr>
2016-11-08 08:52:35 +0100

    add exam file

---
+++ exam.m
@@ -0,0 +1,21 @@
+clear all
+
+% Problem 1
+
+% a= [2.59 12 69 32];
+% b= [2.74 9.3 2 79]';
+% c= [12:-0.5:-12]';
+% A= ones(10)+9;
+% v=[5 4 3 2 1 2 3 4 5];
+% v= zeros(5, 4, 3, 2, 1, 2, 3, 4, 5)
+% x= size(v)
+% B=diag(x)
+% D=randi([-100,100],3,3);
+
+%Problem 2
+theta= [0;360];
+a=1;
+b=1.5;
+c=0.3;
+s=a*cos(theta)+sqrt(b.^2-(a*sin(theta)-c).^2)
+plot (theta,s)
